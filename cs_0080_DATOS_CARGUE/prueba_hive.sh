e58311ff4ec698ceed9e13b79669f7c23f54a430
Jpereir <106350975+Jpereir@users.noreply.github.com>
2022-07-14 11:36:10 -0500

    Creacion de consulta con variables

--- cs_0080_DATOS_CARGUE/prueba_hive.sh
+++ cs_0080_DATOS_CARGUE/prueba_hive.sh
@@ -14,12 +14,7 @@ V_CONF_SPARK="spark-sql --driver-memory 15G --executor-memory 15G --executor-cor
 V_0080_DATOS_CARGA_TABLAS='tbl_fact_datos_trafico,tbl_datos_control_cargue'
 
 
-spark-sql --driver-memory 15G --executor-memory 15G --executor-cores 15 --num-executors 15 --queue 0080_0023_ACDRS --name '0080_DATOS_CONCATENAR' -e "Select cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci, SUM(uplink), SUM(DOWNLINK) from datos.tbl_fact_datos_trafico where fecha_trafico = '20220302' and plmnidentifier  NOT LIKE '732%'AND uplink  IS NOT NULL  AND downlink  IS NOT NULL AND val_qci  IS NOT NULL group by cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci LIMIT 1" 
-
-
-V_FECHA="CAST(current_date- interval 1 day  as date)"
-echo $V_FECHA
-spark-sql --driver-memory 15G --executor-memory 15G --executor-cores 15 --num-executors 15 --queue 0080_0023_ACDRS --name '0080_DATOS_CONCATENAR' -e "Select cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci, SUM(uplink), SUM(DOWNLINK) from datos.tbl_fact_datos_trafico where fecha_trafico = $V_FECHA and plmnidentifier  NOT LIKE '732%' group by cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci"
+spark-sql --driver-memory 15G --executor-memory 15G --executor-cores 15 --num-executors 15 --queue 0080_0023_ACDRS --name '0080_DATOS_CONCATENAR' -e "Select cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci, SUM(uplink), SUM(DOWNLINK) from datos.tbl_fact_datos_trafico where fecha_trafico = '20220302' and plmnidentifier  NOT LIKE '732%'AND uplink  IS NOT NULL  AND downlink  IS NOT NULL AND val_qci  IS NOT NULL group by cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci " 
 
 
 --------------query con fecha dia anterior
@@ -40,3 +35,13 @@ group by cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci
 
 spark-sql --driver-memory 15G --executor-memory 15G --executor-cores 15 --num-executors 15 --queue 0080_0023_ACDRS --name '0080_DATOS_CONCATENAR' -e "Select cast(record_opening_time as date) as SK_FEC_TRAFICO, apnnetwork as SK_APN, plmnidentifier as SK_PLMNIDENTIFIER, val_qci as SK_QCI, SUM(uplink) as VAL_BYTES_UPLINK,
 SUM(DOWNLINK) as VAL_BYTES_DOWNLINK from datos.tbl_fact_datos_trafico where fecha_trafico = regexp_replace(to_date((date_sub(current_date, 1))),'-','') and plmnidentifier  NOT LIKE '732%' and uplink  IS NOT NULL and downlink  IS NOT NULL and val_qci IS NOT NULL group by cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci"
+
+
+
+# Creacion de consulta utilizando variables 
+
+V_CONF_SPARK="spark-sql --driver-memory 15G --executor-memory 15G --executor-cores 9 --num-executors 15 --queue 0080_0023_ACDRS --name '0080_DATOS_CONCATENAR' -e "
+V_FECHA="regexp_replace(to_date((date_sub(current_date, 1))),'-','') "
+echo $V_FECHA
+$V_CONF_SPARK "Select cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci, SUM(uplink), SUM(DOWNLINK) from datos.tbl_fact_datos_trafico where fecha_trafico = $V_FECHA and plmnidentifier  NOT LIKE '732%' group by cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci"
+

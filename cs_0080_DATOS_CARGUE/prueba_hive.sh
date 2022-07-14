808f6864aa3a6a5b1c1c8542f5b153cce075ddaa
Jpereir <106350975+Jpereir@users.noreply.github.com>
2022-07-14 09:48:32 -0500

    Creacion variable fecha

--- cs_0080_DATOS_CARGUE/prueba_hive.sh
+++ cs_0080_DATOS_CARGUE/prueba_hive.sh
@@ -13,4 +13,10 @@ V_SPARK_WAREHOUSE_DIR="/apps/spark/warehouse"
 V_CONF_SPARK="spark-sql --driver-memory 15G --executor-memory 15G --executor-cores 9 --num-executors 15 --queue 0080_0023_ACDRS --name '0080_DATOS_CONCATENAR' -e "
 V_0080_DATOS_CARGA_TABLAS='tbl_fact_datos_trafico,tbl_datos_control_cargue'
 
+
 spark-sql --driver-memory 15G --executor-memory 15G --executor-cores 15 --num-executors 15 --queue 0080_0023_ACDRS --name '0080_DATOS_CONCATENAR' -e "Select cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci, SUM(uplink), SUM(DOWNLINK) from datos.tbl_fact_datos_trafico where fecha_trafico = '20220302' and plmnidentifier  NOT LIKE '732%'AND uplink  IS NOT NULL  AND downlink  IS NOT NULL AND val_qci  IS NOT NULL group by cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci LIMIT 1" 
+
+
+V_FECHA="CAST(current_date- interval 1 day  as date)"
+echo $V_FECHA
+spark-sql --driver-memory 15G --executor-memory 15G --executor-cores 15 --num-executors 15 --queue 0080_0023_ACDRS --name '0080_DATOS_CONCATENAR' -e "Select cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci, SUM(uplink), SUM(DOWNLINK) from datos.tbl_fact_datos_trafico where fecha_trafico = $V_FECHA and plmnidentifier  NOT LIKE '732%' group by cast(record_opening_time as date), apnnetwork, plmnidentifier, val_qci"

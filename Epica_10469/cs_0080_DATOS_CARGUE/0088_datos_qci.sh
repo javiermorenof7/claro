e47c589dc291b3b7927830bce4ed3ee03e88cd2b
javiermorenof7 <94337664+javiermorenof7@users.noreply.github.com>
2022-09-05 14:41:14 -0500

    elimina y actualizacion de sh

--- Epica_10469/cs_0080_DATOS_CARGUE/0088_datos_qci.sh
+++ Epica_10469/cs_0080_DATOS_CARGUE/0088_datos_qci.sh
@@ -17,7 +17,7 @@ START=$(date +%s)
 SCRIPT=$(readlink -f $0);
 path_proceso=`dirname $SCRIPT`;
 ext="txt"
-temp_folder="TEMPORAL"
+temp_folder="06_LOGS"
 id_ejecucion=${1}
 
 /usr/jdk64/jdk1.8.0_112/bin/java -jar /DWH/99_ADMINISTRACION/0091_ADMON_SISNOT/02_BATS/0091_extrae_parametros_prm_comcel.jar qa JP_0080_DATOS_QCI txt /DWH/DESARROLLO_DWH/02_DATOS/0080_DATOS_CARGUE_QCI/03_FUENTES/0080_VARIABLES_SISNOT.txt
@@ -37,6 +37,7 @@ main_class="com.claro.App"
 jar_name="0080_DATOS_CARGUE_QCI-assembly-1.0.jar"
 queue="OTROS"
 #queue="PROCESOS_DIARIOS"
+path_fuentes_proyecto="/DWH/DESARROLLO_DWH/02_DATOS/0080_DATOS_CARGUE_QCI"
 path_fuentes="/DWH/DESARROLLO_DWH/02_DATOS/0080_DATOS_CARGUE_QCI/03_FUENTES"
 database="datos"
 echo "Inicio de proceso "${job_name}
@@ -58,7 +59,7 @@ ambiente_sisnot_notificaciones="dev"
 #ambiente_sisnot_notificaciones="pr"
 path_extrae_pr="/DWH/99_ADMINISTRACION/0091_ADMON_SISNOT/02_BATS/0091_extrae_parametros_prm_comcel.jar"
 path_ejecuta_escenario="/DWH/99_ADMINISTRACION/0091_ADMON_SISNOT/02_BATS/0091_ejecuta_escenario_sisnot.jar"
-path_log=${path_fuentes}/${temp_folder}/${app_name}_`date +\%Y\%m\%d\%H\%M`.log
+path_log=${path_fuentes_proyecto}/${temp_folder}/${app_name}_`date +\%Y\%m\%d\%H\%M`.log
 #****************************
 #Configuración Fecha Proceso
 #****************************
@@ -72,8 +73,13 @@ echo "Fecha carga: "${fecha_YYYYMMDD}
 #****************************
 spark-submit --queue ${queue} --master yarn --class ${main_class} --num-executors ${executors} --executor-cores ${cores} --executor-memory ${executorMemory} --driver-memory ${driverMemory} --conf spark.port.maxRetries=100 --conf spark.executor.memoryOverhead=24336 --conf spark.driver.maxResultSize=4g --conf spark.default.parallelism=3000 --conf spark.sql.shuffle.partitions=3000 --conf spark.sql.hive.hiveserver2.jdbc.url="jdbc:hive2://tfm2044-hdpcmtr03.claro.co:2181,tfm2403-hdpcmtr04.claro.co:2181,tfm2404-hdpcmtr05.claro.co:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2" --conf spark.datasource.hive.warehouse.metastoreUri=thrift://tfm2403-hdpcmtr04.claro.co:9083,thrift://tfm2044-hdpcmtr03.claro.co:9083 --conf spark.hadoop.hive.llap.daemon.service.hosts=@llap0 --conf spark.hadoop.hive.zookeeper.quorum=tfm2044-hdpcmtr03.claro.co:2181,tfm2403-hdpcmtr04.claro.co:2181,tfm2404-hdpcmtr05.claro.co:2181 --conf spark.hadoop.metastore.catalog.default=hive  --conf spark.datasource.hive.warehouse.load.staging.dir=/tmp --conf spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.ui.port=4049 ${path_proceso}/${jar_name} --app_name ${app_name} --job_name ${job_name} --path_extrae_pr ${path_extrae_pr} --path_ejecuta_escenario ${path_ejecuta_escenario} --ext ${ext} --temp_folder ${temp_folder} --sisnot_repositorio ${sisnot_repositorio} --ambiente_sisnot_variables ${ambiente_sisnot_variables} --ambiente_sisnot_notificaciones ${ambiente_sisnot_notificaciones} --path_fuentes ${path_fuentes} --fecha_proceso ${fecha_YYYYMMDD} --id_ejecucion 1 --database ${database} --V_DIAS_ATRASO ${V_DIAS_ATRASO} #>> ${path_log}
 
+echo 'Fin proceso Spark' #>> $path_log
 
-#echo 'Fin proceso Spark' >> $path_log
+#****************************
+#Eliminacion de Logs
+#****************************
+find ${path_fuentes_proyecto}/${temp_folder}/* -mtime +7 -exec rm {} \;
+#find /DWH/DESARROLLO_DWH/02_DATOS/0080_DATOS_CARGUE_QCI/06_LOGS/* -mtime +7 -exec rm {} \;
 
 END=$(date +%s)
 DIFF=$(( $END - $START ))
